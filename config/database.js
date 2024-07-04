d21c94e8aba75efd25c136c7e9df6ea7b4938528
komal <kk6612465@gmail.com>
2024-07-04 22:43:03 +0530

    Project Done

--- config/database.js
+++ config/database.js
@@ -11,23 +11,3 @@ mongoose
     console.log(error.message);
     process.exit(1);
   });
-
-// const mongoose = require("mongoose");
-// require("dotenv").config();
-
-// const URI = process.env.MONGO_DB;
-
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(URI, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
-//     console.log("MongoDB Connected");
-//   } catch (error) {
-//     console.error("MongoDB Connection Error:", error.message);
-//     process.exit(1);
-//   }
-// };
-
-// module.exports = connectDB;
